80cad722ac25f0cb1e4d5641b0c2d78dd2c72688
Jacek Grzybowski <totospl99@gmail.com>
2022-08-06 00:29:08 +0200

    graph generation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::time::{Duration, Instant};
+
+use indicatif::ProgressBar;
+
+struct NodeGraph<'a> {
+    node: &'a String,
+    paths: Vec<&'a String>,
+}
 
 fn main() {
     let file = File::open("words_alpha.txt").expect("Failed to open words_alpha file");
@@ -9,12 +18,11 @@ fn main() {
     let words_five_letter = remove_repeat_letter_words(words_five_letter);
     println!("without repeating letters: {}", words_five_letter.len());
 
+    let sorted_map = sort_anagrams(&words_five_letter);
+    println!("with reduced anagrams: {}", sorted_map.len());
+    // println!("{:?}", sorted_map.into_iter().next());
 
-    for i in (0..words_five_letter.len()).step_by(words_five_letter.len() / 20) {
-        println!("{:}", words_five_letter[i]);
-    }
-
-    println!("Hello, world!");
+    let graph = create_graph(&sorted_map);
 }
 
 fn get_all_five_words(file: File) -> Vec<String> {
@@ -40,6 +48,70 @@ fn word_diff_letters(word: &String) -> bool {
         }
     }
     true
+}
+
+fn sort_anagrams(words: &Vec<String>) -> HashMap<String, Vec<String>> {
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+    for word in words {
+        let mut sorted_vec: Vec<char> = word.chars().collect();
+        sorted_vec.sort_unstable();
+        let sorted: String = sorted_vec.into_iter().collect();
+
+        map.entry(sorted)
+            .and_modify(|v| v.push(word.clone()))
+            .or_insert(vec![word.clone()]);
+    }
+    map
+}
+
+fn create_graph<'a>(map: &'a HashMap<String, Vec<String>>) -> Vec<NodeGraph> {
+    println!("Creating graph...");
+    let start = Instant::now();
+    let pb = ProgressBar::new(map.len().try_into().unwrap());
+
+    let mut graph: Vec<NodeGraph> = Vec::new();
+
+    let keys = Vec::from_iter(map.into_iter().map(|e| e.0));
+
+    // Graph must be symmetric, because five word series is symmetric.
+    // So to remove loops in graph we dont check words that was inserted into graph.
+    for (i, k1) in keys.iter().enumerate() {
+        let mut paths: Vec<&String> = Vec::new();
+        for j in i + 1..keys.len() {
+            let k2 = keys[j];
+            if !check_if_words_cover(k1, k2) {
+                paths.push(k2);
+            }
+        }
+        let node = NodeGraph { node: k1, paths };
+        graph.push(node);
+        pb.inc(1);
+    }
+
+    pb.finish();
+    println!("Done. Time elapsed: {:?}", start.elapsed());
+    graph
+}
+
+fn check_if_words_cover(w1: &String, w2: &String) -> bool {
+    for i in 0..w1.len() {
+        let l1 = w1.chars().nth(i).unwrap();
+        for j in 0..w2.len() {
+            let l2 = w2.chars().nth(j).unwrap();
+            if l1 == l2 {
+                return true;
+            }
+        }
+    }
+    false
+}
+
+fn find_five_unique(graph: Vec<NodeGraph>) -> Vec<Vec<&String>> {
+    let mut unique: Vec<Vec<&String>> = Vec::new();
+
+    // let mut discovered: Ha
+
+    unique
 }
 
 #[cfg(test)]
