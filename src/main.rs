f9e3f1b0241f118fbb872df0f52173a7d2bd0f9f
Jacek Grzybowski <totospl99@gmail.com>
2022-12-30 15:50:07 +0100

    repeat char remove

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
-
-use indicatif::ProgressBar;
 
 // const WORD_FILENAME: &str = "words_alpha.txt";
 const WORD_FILENAME: &str = "words_5.txt"; // used for now for faster debug
 const OUT_FILENAME: &str = "cliques.txt";
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz";
 
 fn main() {
     // load file with words and take every with five letters
-    let worlds_five: Vec<String> =
+    println!("Load file...");
+    let mut worlds_five: Vec<String> =
         io::BufReader::new(File::open(WORD_FILENAME).expect("Could not find input file."))
             .lines()
             .filter_map(|x| x.ok())
             .filter(|x| x.len() == 5)
+            .map(|x| x.to_lowercase())
             .collect();
+    println!("Amount of words:\t{:?}", worlds_five.len());
 
-    print!("{:?}", worlds_five);
+    println!("Filter words with repeating characters...");
+    worlds_five.retain(|x| !word_repeat_char(x, 5));
+    println!("Amount of words:\t{:?}", worlds_five.len());
+
+    println!("Map words to u32 representative...");
+
+    // println!("{:?}", worlds_five);
+}
+
+fn word_repeat_char(w: &str, word_size: usize) -> bool {
+    let mut map: HashMap<char, bool> = HashMap::with_capacity(word_size);
+    for c in w.chars() {
+        if map.get(&c).is_some() {
+            return true;
+        }
+        map.insert(c, true);
+    }
+    false
 }
