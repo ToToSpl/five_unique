6c1970d557b0e6a9571ba49538bd30a7797f874d
Jacek Grzybowski <totospl99@gmail.com>
2022-12-30 14:52:21 +0100

    words loading

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, BufRead, Error, Write};
-use std::time::Instant;
+use std::io::{self, BufRead};
 
 use indicatif::ProgressBar;
 
-const WORD_FILENAME: &str = "words_alpha.txt";
-const GRAPH_FILENAME: &str = "graph.csv";
+// const WORD_FILENAME: &str = "words_alpha.txt";
+const WORD_FILENAME: &str = "words_5.txt"; // used for now for faster debug
 const OUT_FILENAME: &str = "cliques.txt";
 
-fn main() {}
+fn main() {
+    // load file with words and take every with five letters
+    let worlds_five: Vec<String> =
+        io::BufReader::new(File::open(WORD_FILENAME).expect("Could not find input file."))
+            .lines()
+            .filter_map(|x| x.ok())
+            .filter(|x| x.len() == 5)
+            .collect();
+
+    print!("{:?}", worlds_five);
+}
